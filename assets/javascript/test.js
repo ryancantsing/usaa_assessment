057b23b855fbe40c2270f8523ce0252689459089
unknown <Ryan Riddell>
2019-05-20 00:22:16 -0700

    Did a little bit of cleanup

--- assets/javascript/test.js
+++ assets/javascript/test.js
@@ -1,14 +1,4 @@
 var districts = [112, 98, 119, 126, 129, 44, 118, 125, 101, 27]
-// function theDistricts(districts){
-//     var total = 0
-//     for(var x = 0; x <= districts.length - 1; x++){
-//         total += districts[x]
-//         var bar = "<div id='stretchyBar " + (x + 1) + "' value=" + districts[x] + " style='height: 20px; width: 5px;> another div </div>"
-//         document.getElementById('putMeIn').innerHTML = document.getElementById('putMeIn').innerHTML + "<p style='font-size: 12px; font-weight: bold'> District " + (x + 1) + ": " + districts[x] + " Pledges </p>" + bar
-//         stretchThatBar(districts[x], "stretchyBar" + (x + 1))
-//     };
-//     return total;
-//     }
 function populateDistricts(districts){
     var total = 0;
     for(var x  = 0; x <= districts.length - 1; x++){
@@ -47,18 +37,4 @@ document.onload = populatePledges(districts)
 document.onload = window.setTimeout(function (){
     stretchyBars(districts)
 }, 100)
-
-// Populate District HTML w/ StretchyBar
-// function populateDistricts(districts){
-//     var total = 0;
-//     for(var x  = 0; x <= districts.length - 1; x++){
-//         total += districts[x];
-//         var mainParagraph = "<p style='font-size: 12px; font-weight: bold'> District " + (x + 1) + ": "
-//         var middleParagraph = "<div id='stretchyBar " + (x + 1) + "' value=" + districts[x] + " style='height: 20px; width: 5px;> another div </div> "
-//         var endParagraph = districts[x] + " Pledges </p>"
-//         console.log(mainParagraph, middleParagraph, endParagraph)
-//         document.getElementById('putMeIn').innerHTML = document.getElementById('putMeIn').innerHTML + mainParagraph, middleParagraph, endParagraph
-//     }
-//     return total
-// }
 
